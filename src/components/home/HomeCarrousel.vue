6452d6646fa1bacd6d36e4430b5d161c80684f48
Zet Almeida <daniel20150154@gmail.com>
2020-09-23 20:30:40 +0100

    Styling Carousel

--- src/components/home/HomeCarrousel.vue
+++ src/components/home/HomeCarrousel.vue
@@ -13,54 +13,34 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
       <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide
-        img-src="https://picsum.photos/1024/480/?image=58"
-      ></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          />
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
-          pellentesque ut lacus vel interdum.
-        </p>
+        v-for="(item, i) in carousel"
+        v-bind:key="i"
+        class="da-carousel-slide"
+        :img-src="item.image"
+      >
+        <div class="da-carousel-caption">
+          <h1 class="da-carousel-caption-header">{{ item.title }}</h1>
+          <p class="da-carousel-caption-content">{{ item.content }}</p>
+        </div>
       </b-carousel-slide>
     </b-carousel>
   </div>
 </template>
 <script>
 export default {
+  props: ['carouselProp'],
   data() {
     return {
       slide: 0,
       sliding: null,
+      carouselData: [
+        {
+          image: require('../../assets/imgs/salao.jpg'),
+          title: 'Encontre tudo que precisa para o seu evento',
+          content: `Diga-nos o serviço de que precisa, receba orçamentos, contrate os fornecedores adequados e organize o seu evento sem sair de casa`,
+        },
+      ],
     }
   },
   methods: {
@@ -71,8 +51,34 @@ export default {
       this.sliding = false
     },
   },
+  computed: {
+    carousel() {
+      return this.carouselProp || this.carouselData
+    },
+  },
+  created() {},
+  mounted() {
+    document.getElementsByClassName('carousel-caption').forEach((element) => {
+      element.setAttribute('style', 'bottom: 30%; left: 12%;')
+    })
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.da-carousel-caption {
+  width: 60%;
+}
+.da-carousel-caption .da-carousel-caption-header,
+.da-carousel-caption .da-carousel-caption-content {
+  text-align: left;
+}
+
+.da-carousel-caption .da-carousel-caption-header {
+  font-weight: 500;
+}
+
+.da-carousel-caption .da-carousel-caption-content {
+  font-size: 1.2rem;
+}
+</style>
