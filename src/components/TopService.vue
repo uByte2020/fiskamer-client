a951be0eac9b64e1452a2eef8dbad0eb2935a319
Zet Almeida <daniel20150154@gmail.com>
2020-09-24 22:35:33 +0100

    Add Serviços em Destaque - Starting

---
+++ src/components/TopService.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="top-service"></div>
+</template>
+
+<script>
+export default {
+  name: 'TopService',
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import HelloWorld from './HelloWorld.vue'
 import HomeCarrousel from './home/HomeCarrousel.vue'
 import HomeSearch from './home/HomeSearch.vue'
 import Header from './Header.vue'
+import TopService from './TopService.vue'
 
-export { HelloWorld, Header, HomeCarrousel, HomeSearch }
+export { HelloWorld, Header, HomeCarrousel, HomeSearch, TopService }
